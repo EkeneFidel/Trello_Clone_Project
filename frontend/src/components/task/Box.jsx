3f251c070df5d86644dec785cc1b4735704c7634
Priince <tobiloba110@gmail.com>
2023-03-21 03:17:42 +0100

    feat: Add changes in frontend directory - Implement routing to login page after signup - Implement delete task functionality - Implement edit task functionality - Implement generation of random task id

--- frontend/src/components/task/Box.jsx
+++ frontend/src/components/task/Box.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './box.css'
 
-export default function Box({ content, setBoxArrays, handleDelete, id }) {
+export default function Box({ content, setBoxArrays, id }) {
     const [text, setText] = useState(content);
     const [showTrash, setShowTrash] = useState(false);
     function handleShowTrash() {
@@ -11,18 +11,22 @@ export default function Box({ content, setBoxArrays, handleDelete, id }) {
         setText(event.target.value);
         setBoxArrays((prev) => {
             return prev.map((item) => {
-                if (item.text === content) {
+                if (item.id === id) {
                     return { ...item, text: event.target.value }
                 }
                 return item
             })
         })
     }
+    function handleDelete(id) {
+        setBoxArrays((prev) =>{
+            return prev.filter((item) => item.id !== id)})
+    }
     
   return (
     <div className='box' onMouseEnter={handleShowTrash} onMouseLeave={handleShowTrash} >
-        <i className={ showTrash ? 'bx bxs-trash show-trash' : 'bx bxs-trash' } onClick={(id) => handleDelete(id)}></i>
-        <textarea className='box-textarea' value={text} onChange={(event) => setText(event.target.value)} >
+        <i className={ showTrash ? 'bx bxs-trash show-trash' : 'bx bxs-trash' } onClick={() => handleDelete(id)}></i>
+        <textarea className='box-textarea' value={text} onChange={handleContentChange} >
         </textarea>
 
     </div>
--- frontend/src/pages/signup/Signup.jsx
+++ frontend/src/pages/signup/Signup.jsx
@@ -1,35 +1,40 @@
 import { useState } from 'react'
-import { Link, redirect } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import './signup.css'
 
 export default function Signup() {
-  // const [user, setUser] = useState({});
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const navigate = useNavigate();
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+  const [loading, setLoading] = useState(false);
+  const backendUrl = 'http://localhost:3000';
   function handleSubmit(event) {
     event.preventDefault();
-    if (password !== confirmPassword) {
+    if (user.password !== user.confirmPassword) {
       alert('Passwords do not match')
       return
     }
-    fetch('/api/v1/auth/signup', {
+    setLoading(true);
+    fetch(`${backendUrl}/api/v1/auth/signup`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        email,
-        password,
-        confirmPassword,
+        email: user.email,
+        password: user.password,
+        
       }),
     })
     .then((res) => {
       if (res.status === 200) {
-        setEmail('');
-        setPassword('');
-        setConfirmPassword('');
-        redirect("/login");
+        setLoading(false);
+        setTimeout(() => {
+          navigate('/login');
+        }, 1000)
         return res.json();
       }
       throw new Error('Something went wrong');
@@ -46,10 +51,10 @@ export default function Signup() {
     <div className='signup'>
         <form className='signup-form' onSubmit={handleSubmit}>
             <h1 className='signup-title'>Sign up</h1>
-            <input type='email' value={email} onChange={(e) => setEmail(e.target.value)} className='signup-input' placeholder='Email address' required/>
-            <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} className='signup-input' placeholder='Enter Password' required/>
-            <input type='password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} className='signup-input' placeholder='Confirm Password' required/>
-            <button className='signup-btn'>Create account</button>
+            <input type='email' value={user.email} onChange={(e) => setUser({...user, email: e.target.value})} className='signup-input' placeholder='Email address' required/>
+            <input type='password' value={user.password} onChange={(e) => setUser({...user, password: e.target.value})} className='signup-input' placeholder='Enter Password' pattern='(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}' title='Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters' required/>
+            <input type='password' value={user.confirmPassword} onChange={(e) => setUser({...user, confirmPassword: e.target.value})} className='signup-input' placeholder='Confirm Password' required/>
+            <button className='signup-btn'>{ loading ? 'Loading...' : 'Create account' }</button>
         </form>
         <footer>
             <small>Already have an account? <Link to='/login'><span>Log in</span></Link></small>
