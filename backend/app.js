a675a92f815ec783432158e4516dec5aa06488ea
EkeneFidel <echukwurah99@gmail.com>
2023-03-23 13:04:01 +0100

    feat: add lists endpoints

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,21 @@
-const express = require("express")
+const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors")
+const cors = require("cors");
 require("dotenv").config();
 
 const db = require("./config/db.config");
-const authRouter = require("./routes/auth.routes")
+const authRouter = require("./routes/auth.routes");
+const listRouter = require("./routes/list.routes");
 
 db.connectToMongoDB();
-const PORT =  process.env.PORT
-const app = express()
+const PORT = process.env.PORT;
+const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use("/api/v1/list", listRouter);
 app.use("/api/v1/auth", authRouter);
 app.get("/api/v1/", (req, res) => {
     res.send("Pella Yay!");
@@ -21,7 +23,6 @@ app.get("/api/v1/", (req, res) => {
 app.get("/", (req, res) => {
     res.send("Pella Yay!");
 });
-
 
 // Handle errors.
 app.use(function (err, req, res, next) {
@@ -34,5 +35,4 @@ app.listen(PORT, () => {
     console.log(`Listening on port: ${PORT}`);
 });
 
-
-module.exports = app
+module.exports = app;
